72f847b875317a5c64dd6391185517bbcbe1ecf7
Sora Matta Machado <soramattamachado@gmail.com>
2021-05-18 21:50:21 -0300

    Add font and some css

--- App.js
+++ App.js
@@ -2,9 +2,20 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Nunito_400Regular as NunitoRegular
+} from '@expo-google-fonts/nunito';
 import MainNavigator from './navigation/mainNavigator';
 
 const App = () => {
+  let [fontsLoaded] = useFonts({
+    NunitoRegular
+  });
+
+  if (!fontsLoaded) return <AppLoading />
+
   return (
     <View style={styles.container}>
       <NavigationContainer>
@@ -17,6 +28,8 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
+    height: '100%',
     backgroundColor: '#fff'
   },
 });
--- navigation/mainNavigator.js
+++ navigation/mainNavigator.js
@@ -12,7 +12,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent:'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
   }
 });
 
