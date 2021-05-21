87c11127cb49966c0af5237475b77fec2a944ac6
Diego C. Rodrigues <diego.rodrigues@growtisolutions.com>
2021-05-20 23:27:32 -0300

    feat: Dashboard: Home Page

---
+++ components/CategoryProducts.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, Text, Image } from 'react-native';
+import { Card, } from 'react-native-paper';
+const CategoryProducts = (props) => {
+    const { Products } = props;
+    return (
+        <>
+            {Products.map((product) =>
+                <Card key={product.id} style={styles.card} >
+                    <Card.Content style={styles.CardContent}>
+                        <Image style={{ width: 140, height: 100 }} source={{ uri: `${product.ProductImage}` }}></Image>
+                        <Text style={styles.CardTitleText}>{product.ProductTitle}</Text>
+                    </Card.Content>
+                </Card >
+            )}
+        </>
+
+    )
+}
+const styles = StyleSheet.create({
+    card: {
+        width: 340,
+        height: 130,
+        resizeMode: 'center',
+        marginRight: 20,
+        padding: 2,
+    },
+
+    CardTitleText: {
+        fontFamily: 'NunitoRegular',
+        fontSize: 20,
+        marginRight: 145,
+        marginLeft: 10,
+    },
+    CardContent: {
+        flexDirection: "row",
+        alignItems: "flex-start",
+
+    }
+});
+
+export default CategoryProducts
