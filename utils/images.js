968013064ca13e4631c796e7baa972946ce6a912
Sora Matta Machado <soramattamachado@gmail.com>
2021-05-21 00:01:17 -0300

    Add purchace history screen

---
+++ utils/images.js
@@ -0,0 +1,6 @@
+const Images = {
+  camarao_costa_negra_2: require('../assets/camarão_costa_negra_2.jpg'),
+  chapada_diamantina_3: require('../assets/chapada_diamantina_3.png'),
+};
+
+export default Images;
