a884be64148cfbe10d1948e7e562cd02ac71a288
Sora Matta Machado <soramattamachado@gmail.com>
2021-05-22 16:04:05 -0300

    Profile screen updated

--- utils/images.js
+++ utils/images.js
@@ -1,7 +1,8 @@
 const Images = {
   camarao_costa_negra_2: require('../assets/camarão_costa_negra_2.jpg'),
   chapada_diamantina_3: require('../assets/chapada_diamantina_3.png'),
-  money: require('../assets/money_icon.png')
+  money: require('../assets/money_icon.png'),
+  default_avatar: require('../assets/default_avatar.png')
 };
 
 export default Images;
