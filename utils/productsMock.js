f045535d7b57c8f953e60ec350295fa3863b1d9b
Sora Matta Machado <soramattamachado@gmail.com>
2021-09-21 19:58:28 -0300

    update products

--- utils/productsMock.js
+++ utils/productsMock.js
@@ -1,306 +1,174 @@
-const ProductsMock =
-    [{
+const ProductsMock = [
+    {
         "id": 1,
         "CategoryTitle": "Alimentos",
         "Products": [{
             "id": 1,
-            "ProductImage": "https://i.imgur.com/Yq6WDVo.jpg",
-            "ProductTitle": "Hambúrguer de cacau",
+            "ProductImage": "https://i.imgur.com/uPM5BWJ.jpg",
+            "ProductTitle": "Castanha de baru torrada",
             "ProductValue": "24,00"
         },
         {
             "id": 2,
-            "ProductImage": "https://i.imgur.com/ZECyZ69.jpg",
-            "ProductTitle": "Doce de endocarpo cacau",
+            "ProductImage": "https://i.imgur.com/6rYZ3NL.jpeg",
+            "ProductTitle": "Conserva de pequi",
             "ProductValue": "10,00"
         },
         {
             "id": 3,
-            "ProductImage": "https://i.imgur.com/nlzooM3.png",
-            "ProductTitle": "Suco mel de cacau",
+            "ProductImage": "https://i.imgur.com/XtXGu8Y.jpg",
+            "ProductTitle": "Farinha de pequi",
             "ProductValue": "9,00"
 
         },
         {
             "id": 4,
-            "ProductImage": "https://i.imgur.com/uqrnsAy.jpg",
-            "ProductTitle": "Frutas desidratadas com chocolate",
+            "ProductImage": "https://i.imgur.com/6qMytHd.jpg",
+            "ProductTitle": "Geleia de buriti",
             "ProductValue": "15,00"
 
         },
         {
             "id": 5,
-            "ProductImage": "https://i.imgur.com/ooRHL0y.jpg",
-            "ProductTitle": "Iogurte de cacau",
+            "ProductImage": "https://i.imgur.com/EcuW90v.jpg",
+            "ProductTitle": "Licor de pequi",
             "ProductValue": "7,00"
 
         },
         {
             "id": 6,
-            "ProductImage": "https://i.imgur.com/886mYpr.jpg",
-            "ProductTitle": "Geleia de cacau",
+            "ProductImage": "https://i.imgur.com/epoFwEu.jpg",
+            "ProductTitle": "Suco de buriti",
             "ProductValue": "14,00"
 
-        },
-        {
-            "id": 7,
-            "ProductImage": "https://i.imgur.com/ezDOq54.jpg",
-            "ProductTitle": "Cacau em pó",
-            "ProductValue": "20,00"
-
-        },
-        {
-            "id": 8,
-            "ProductImage": "https://i.imgur.com/WRvNsAv.jpg",
-            "ProductTitle": "Nibs de cacau",
-            "ProductValue": "18,00"
-
-        },
-        {
-            "id": 9,
-            "ProductImage": "https://i.imgur.com/orPlBiW.jpg",
-            "ProductTitle": "Carpaccio de chocolate",
-            "ProductValue": "26,00"
-
-        },
-        {
-            "id": 10,
-            "ProductImage": "https://i.imgur.com/r2rK05H.jpg",
-            "ProductTitle": "Chocolate 70%",
-            "ProductValue": "17,00"
-
-        },
-        {
-            "id": 11,
-            "ProductImage": "https://i.imgur.com/0OzYgcL.jpg",
-            "ProductTitle": "Chocolate 60%",
-            "ProductValue": "16,00"
-
-        },
-        {
-            "id": 12,
-            "ProductImage": "https://i.imgur.com/HbXKXtB.jpg",
-            "ProductTitle": "Chocolate 80%",
-            "ProductValue": "19,00"
-
-        },
-        {
-            "id": 13,
-            "ProductImage": "https://i.imgur.com/SxDjK0B.jpg",
-            "ProductTitle": "Doce de banana com cacau",
-            "ProductValue": "11,00"
-
-        },
-        {
-            "id": 14,
-            "ProductImage": "https://i.imgur.com/t0mDsg8.png",
-            "ProductTitle": "Cerveja com cacau",
-            "ProductValue": "16,00"
-
-        },
-        {
-            "id": 15,
-            "ProductImage": "https://i.imgur.com/P9QzpHb.jpg",
-            "ProductTitle": "Cobertura de chocolate",
-            "ProductValue": "12,00"
-
-        },
-        {
-            "id": 16,
-            "ProductImage": "https://i.imgur.com/VBspfTD.jpg",
-            "ProductTitle": "Pão com cacau",
-            "ProductValue": "8,00"
         }
         ]
     },
     {
         "id": 2,
         "CategoryTitle": "Artesanatos",
-        "Products": [{
-            "id": 1,
-            "ProductImage": "https://i.imgur.com/DGHDh0U.jpg",
-            "ProductTitle": "Brinquedo caminhãozinho de madeira",
-            "ProductValue": "26,00"
-        },
-        {
-            "id": 2,
-            "ProductImage": "https://i.imgur.com/YrQoWQq.jpg",
-            "ProductTitle": "Celulose",
-            "ProductValue": "55,00"
-        },
-
-        {
-            "id": 3,
-            "ProductImage": "https://i.imgur.com/INU0rfy.jpg",
-            "ProductTitle": "Escultura de madeira",
-            "ProductValue": "15,00"
-        },
-        {
-            "id": 4,
-            "ProductImage": "https://i.imgur.com/tivuJ9v.jpg",
-            "ProductTitle": "Kit talheres de madeira",
-            "ProductValue": "18,00"
-        },
-        {
-            "id": 5,
-            "ProductImage": "https://i.imgur.com/uT98KSe.jpg",
-            "ProductTitle": "Móvel aparador de madeira",
-            "ProductValue": "85,00"
-        },
-        {
-            "id": 6,
-            "ProductImage": "https://i.imgur.com/VhEZmOS.jpg",
-            "ProductTitle": "Quadro de madeira live your life",
-            "ProductValue": "15,00"
-        },
-        {
-            "id": 7,
-            "ProductImage": "https://i.imgur.com/cXsHWKl.jpg",
-            "ProductTitle": "Semente de cacau",
-            "ProductValue": "3,00"
-        },
-
-
+        "Products": [
+            {
+                "id": 1,
+                "ProductImage": "https://i.imgur.com/bn7Zmw9.jpg",
+                "ProductTitle": "Camisa tingida com tinta de pequi",
+                "ProductValue": "26,00"
+            },
+            {
+                "id": 2,
+                "ProductImage": "https://i.imgur.com/38qnegv.jpg",
+                "ProductTitle": "Carvão ecológico de resíduo de macaúba",
+                "ProductValue": "55,00"
+            },
+            {
+                "id": 3,
+                "ProductImage": "https://i.imgur.com/EvDX0t7.jpg",
+                "ProductTitle": "Corda para colares em palha de buriti",
+                "ProductValue": "15,00"
+            },
+            {
+                "id": 4,
+                "ProductImage": "https://i.imgur.com/F9oeRyO.jpg",
+                "ProductTitle": "Kit cestas em palha de babaçu",
+                "ProductValue": "18,00"
+            },
+            {
+                "id": 5,
+                "ProductImage": "https://i.imgur.com/hm4usPT.jpg",
+                "ProductTitle": "Quebrador de baru",
+                "ProductValue": "85,00"
+            },
+            {
+                "id": 6,
+                "ProductImage": "https://i.imgur.com/4PypyiS.jpg",
+                "ProductTitle": "Toalha de mesa feita com palha de buriti",
+                "ProductValue": "15,00"
+            }
         ]
     },
     {
         "id": 3,
         "CategoryTitle": "Turismo",
-        "Products": [{
+        "Products": [
+            {
             "id": 1,
-            "ProductImage": "https://i.imgur.com/RWHYAZs.jpg",
-            "ProductTitle": "Arborismo",
+            "ProductImage": "https://i.imgur.com/1Pckm6S.jpg",
+            "ProductTitle": "Café da manhã com ingredientes do Cerrado",
             "ProductValue": "160,00"
-        }, {
-            "id": 2,
-            "ProductImage": "https://i.imgur.com/8S6hpBT.jpg",
-            "ProductTitle": "Escalada em árvore",
-            "ProductValue": "120,00"
-        },
-        {
-            "id": 3,
-            "ProductImage": "https://i.imgur.com/rPatKXI.jpg",
-            "ProductTitle": "Escalada em rede",
-            "ProductValue": "40,00"
-        },
-        {
-            "id": 4,
-            "ProductImage": "https://i.imgur.com/i6kf8iP.png",
-            "ProductTitle": "Passe estrada rota do cacau",
-            "ProductValue": "9,50"
-        },
-        {
-            "id": 5,
-            "ProductImage": "https://i.imgur.com/kOKHM2d.jpg",
-            "ProductTitle": "Observação às aves nativas",
-            "ProductValue": "6,00"
-        },
-        {
-            "id": 6,
-            "ProductImage": "https://i.imgur.com/viDY0kr.jpg",
-            "ProductTitle": "Oficina de manuseio do cacau",
-            "ProductValue": "50,00"
-        },
-        {
-            "id": 7,
-            "ProductImage": "https://i.imgur.com/kelBD6f.jpg",
-            "ProductTitle": "Tirolesa",
-            "ProductValue": "20,00"
-        },
-        {
-            "id": 8,
-            "ProductImage": "https://i.imgur.com/T7VkdRP.jpg",
-            "ProductTitle": "Visita à comunidade quilombola nativa",
-            "ProductValue": "30,00"
-        },
-        {
-            "id": 9,
-            "ProductImage": "https://i.imgur.com/T7VkdRP.jpg",
-            "ProductTitle": "Visita à floresta de cacau",
-            "ProductValue": "27,00"
-        },
-        {
-            "id": 10,
-            "ProductImage": "https://i.imgur.com/8UmMA2b.jpg",
-            "ProductTitle": "Visita às colmeias de abelhas uruçu (sem ferrão) e consumo de mel direto da fonte",
-            "ProductValue": "45,00"
-        },
-        {
-            "id": 11,
-            "ProductImage": "https://i.imgur.com/U5xibya.jpg",
-            "ProductTitle": "Visita à fábrica de chocolate",
-            "ProductValue": "32,00"
-        },
-        {
-            "id": 12,
-            "ProductImage": "https://i.imgur.com/r4P05si.jpg",
-            "ProductTitle": "Pousada Ilhéus",
-            "ProductValue": "40,00"
-        },
-        {
-            "id": 13,
-            "ProductImage": "https://i.imgur.com/djtZiAe.jpg",
-            "ProductTitle": "Hotel Uruçuca",
-            "ProductValue": "70,00"
-
-        }]
-
+            },
+            {
+                "id": 2,
+                "ProductImage": "https://i.imgur.com/CBVX0Va.jpg",
+                "ProductTitle": "Oficina de identificação de plantas do Cerrado",
+                "ProductValue": "120,00"
+            },
+            {
+                "id": 3,
+                "ProductImage": "https://i.imgur.com/oBy6qFh.jpg",
+                "ProductTitle": "Pacote turístico Chapada dos Veadeiros",
+                "ProductValue": "40,00"
+            },
+            {
+                "id": 4,
+                "ProductImage": "https://i.imgur.com/eEt88OK.jpg",
+                "ProductTitle": "Tour guiado Parque Estadual da Serra de Caldas Novas",
+                "ProductValue": "9,50"
+            },
+            {
+                "id": 5,
+                "ProductImage": "https://i.imgur.com/QOY7Wp1.jpg",
+                "ProductTitle": "Visita à comunidade das quebradeiras de côco babaçu",
+                "ProductValue": "6,00"
+            },
+            {
+                "id": 6,
+                "ProductImage": "https://i.imgur.com/o4CdNuw.jpg",
+                "ProductTitle": "Visita à Reserva Extrativista Recanto das Araras",
+                "ProductValue": "50,00"
+            }
+        ]
     },
     {
         "id": 4,
         "CategoryTitle": "Farmacos",
-        "Products": [{
+        "Products": [
+            {
             "id": 1,
-            "ProductImage": "https://i.imgur.com/belKrPZ.jpg",
-            "ProductTitle": "Amido de cacau",
+            "ProductImage": "https://i.imgur.com/ccsMahk.jpg",
+            "ProductTitle": "Condicionador sólido pequi e buriti",
             "ProductValue": "5,00"
-        }, {
-            "id": 2,
-            "ProductImage": "https://i.imgur.com/fMw0CoP.jpg",
-            "ProductTitle": "Bioetanol",
-            "ProductValue": "16,00"
-        }, {
-            "id": 3,
-            "ProductImage": "https://i.imgur.com/ZRQrN7r.jpg",
-            "ProductTitle": "Creme hidratante cacau",
-            "ProductValue": "22,00"
-        }, {
-            "id": 4,
-            "ProductImage": "https://i.imgur.com/HeFzL7P.jpg",
-            "ProductTitle": "Extrato de cacau",
-            "ProductValue": "6,00"
-        }, {
-            "id": 5,
-            "ProductImage": "https://i.imgur.com/6gt0kwx.jpg",
-            "ProductTitle": "Kit óleos essenciais",
-            "ProductValue": "170,00"
-        }, {
-            "id": 6,
-            "ProductImage": "https://i.imgur.com/HA9sBif.jpg",
-            "ProductTitle": "Manteiga de cacau",
-            "ProductValue": "4,00"
-        }, {
-            "id": 7,
-            "ProductImage": "https://i.imgur.com/HMMge12.jpg",
-            "ProductTitle": "Nanocelulose",
-            "ProductValue": "100,00"
-        }, {
-            "id": 8,
-            "ProductImage": "https://i.imgur.com/Sc65xH4.png",
-            "ProductTitle": "Resina cacau",
-            "ProductValue": "29,00"
-        }, {
-            "id": 9,
-            "ProductImage": "https://i.imgur.com/h8XOlIZ.jpg",
-            "ProductTitle": "Shampoo sólido cacau",
-            "ProductValue": "18,00"
-        }, {
-            "id": 10,
-            "ProductImage": "https://i.imgur.com/HX7amJl.jpg",
-            "ProductTitle": "Xilitol",
-            "ProductValue": "10,00"
-        }]
-
-    },
-    ]
+            },
+            {
+                "id": 2,
+                "ProductImage": "https://i.imgur.com/A3ScZvJ.jpg",
+                "ProductTitle": "Óleo de baru em cápsula",
+                "ProductValue": "16,00"
+            },
+            {
+                "id": 3,
+                "ProductImage": "https://i.imgur.com/GBUBDOL.jpg",
+                "ProductTitle": "Óleo de baru para cbelos",
+                "ProductValue": "22,00"
+            },
+            {
+                "id": 4,
+                "ProductImage": "https://i.imgur.com/Pbz7dDj.png",
+                "ProductTitle": "Protetor solar em óleo com óleo de buriti",
+                "ProductValue": "6,00"
+            },
+            {
+                "id": 5,
+                "ProductImage": "https://i.imgur.com/LSmPq1l.jpg",
+                "ProductTitle": "Sabão de pequi",
+                "ProductValue": "170,00"
+            },
+            {
+                "id": 6,
+                "ProductImage": "https://i.imgur.com/tGdkLYY.jpg",
+                "ProductTitle": "Xarope expectorante com óleo de pequi",
+                "ProductValue": "4,00"
+            }
+        ]
+    }
+]
 export default ProductsMock;
